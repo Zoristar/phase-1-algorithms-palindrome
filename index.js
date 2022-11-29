6dde4679e2618510ae68b4503c399a36909d4e91
Zoriana <zoriana.shatkovska@gmail.com>
2022-11-29 22:04:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  word = word.toLowerCase();
+  let reverseWord = word.split('').reverse().join('');
+  if (reverseWord === word) {
+    return true;
+  }
+  return false;    
+  }
+
 
 /* 
   Add your pseudocode here
+  -reverse the word
+  -compare the reversed word with the initial one
+  -if same 
+  return true
+  -if not
+  return false
 */
+
 
 /*
   Add written explanation of your solution here
+  we need to reverse the word in order to check if the original word will be the same as the original one
 */
 
 // You can run `node index.js` to view these console logs
